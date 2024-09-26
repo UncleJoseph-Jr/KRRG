6590bb27976d49a1e8a39945cc537c47b4dc1d44
UncleJoseph-Jr <141295348+UncleJoseph-Jr@users.noreply.github.com>
2024-09-26 19:18:34 +0700

    No Characters on Navbar
    No Characters on Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,34 +21,35 @@ function classNames(...classes: string[]) {
 //   return classes.filter(Boolean).join(' ')
 // }
 
-export default function Home() {
+export default function Example() {
   return (
     <Disclosure as="nav" className="bg-gray-800">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-inset focus:ring-white">
-              <span className="absolute -inset-0.5"/>
+            {/* Mobile menu button*/}
+            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+              <span className="absolute -inset-0.5" />
               <span className="sr-only">Open main menu</span>
-              <Bars3Icon className="block h-6 w-6 group-data-[open]:hidden" />
-              <XMarkIcon className="block h-6 w-6 group-data-[open]:hidden" />
+              <Bars3Icon aria-hidden="true" className="block h-6 w-6 group-data-[open]:hidden" />
+              <XMarkIcon aria-hidden="true" className="hidden h-6 w-6 group-data-[open]:block" />
             </DisclosureButton>
           </div>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex flex-shrink-0 items-center">
-              <img 
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" 
-                alt="9KRRG" 
+              <img
+                alt="Your Company"
+                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
                 className="h-8 w-auto"
               />
             </div>
-            <div className="hidden sm:ml-6 sm:black">
+            <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
                 {navigation.map((item) => (
-                  <a 
+                  <a
                     key={item.name}
-                    href={item.name}
-                    aria-current={item.current ? 'page' : undefined} 
+                    href={item.href}
+                    aria-current={item.current ? 'page' : undefined}
                     className={classNames(
                       item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                       'rounded-md px-3 py-2 text-sm font-medium',
@@ -62,24 +63,24 @@ export default function Home() {
           </div>
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
             <button
-              type="button" 
-              className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:iutline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus-gray-800"
+              type="button"
+              className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
             >
-             <span className="absolute -inset-1.5"/>
-             <span className="sr-only">View notification</span> 
-             <BellIcon className="h-6 w-6" aria-hidden="true"/>
+              <span className="absolute -inset-1.5" />
+              <span className="sr-only">View notifications</span>
+              <BellIcon aria-hidden="true" className="h-6 w-6" />
             </button>
 
             {/* Profile dropdown */}
             <Menu as="div" className="relative ml-3">
               <div>
-                <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-whith focus:ring-offset-2 focus:ring-offset-gray-800">
-                  <span className="absolute -inset-1.5"/>
+                <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                  <span className="absolute -inset-1.5" />
                   <span className="sr-only">Open user menu</span>
-                  <img 
-                    alt="" 
+                  <img
+                    alt=""
                     src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    className="h-8 w-8 rounded-full" 
+                    className="h-8 w-8 rounded-full"
                   />
                 </MenuButton>
               </div>
@@ -88,18 +89,18 @@ export default function Home() {
                 className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
               >
                 <MenuItem>
-                  <a href="#" 
-                  className="block px-4 py-2 text-gray-700 data:bg-gray-100">Your Profile
+                  <a href="#" className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
+                    Your Profile
                   </a>
                 </MenuItem>
                 <MenuItem>
-                  <a href="#" 
-                  className="block px-4 py-2 text-gray-700 data:bg-gray-100">Settings
+                  <a href="#" className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
+                    Settings
                   </a>
                 </MenuItem>
                 <MenuItem>
-                  <a href="#" 
-                  className="block px-4 py-2 text-gray-700 data:bg-gray-100">Sign out
+                  <a href="#" className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
+                    Sign out
                   </a>
                 </MenuItem>
               </MenuItems>
@@ -119,14 +120,13 @@ export default function Home() {
               className={classNames(
                 item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                 'block rounded-md px-3 py-2 text-base font-medium',
-            )}
+              )}
             >
-
+              {item.name}
             </DisclosureButton>
           ))}
         </div>
       </DisclosurePanel>
-
     </Disclosure>
-  );
+  )
 }

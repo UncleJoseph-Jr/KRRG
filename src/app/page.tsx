2411f28bcd921c99c0835a54361db4af44fe694d
UncleJoseph-Jr <141295348+UncleJoseph-Jr@users.noreply.github.com>
2024-09-26 19:07:20 +0700

    add Profile dropdown
    add Profile dropdown

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,13 +61,72 @@ export default function Home() {
             </div>
           </div>
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            <button className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:iutline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus-gray-800"
+            <button
+              type="button" 
+              className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:iutline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus-gray-800"
             >
-              
+             <span className="absolute -inset-1.5"/>
+             <span className="sr-only">View notification</span> 
+             <BellIcon className="h-6 w-6" aria-hidden="true"/>
             </button>
+
+            {/* Profile dropdown */}
+            <Menu as="div" className="relative ml-3">
+              <div>
+                <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-whith focus:ring-offset-2 focus:ring-offset-gray-800">
+                  <span className="absolute -inset-1.5"/>
+                  <span className="sr-only">Open user menu</span>
+                  <img 
+                    alt="" 
+                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    className="h-8 w-8 rounded-full" 
+                  />
+                </MenuButton>
+              </div>
+              <MenuItems
+                transition
+                className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+              >
+                <MenuItem>
+                  <a href="#" 
+                  className="block px-4 py-2 text-gray-700 data:bg-gray-100">Your Profile
+                  </a>
+                </MenuItem>
+                <MenuItem>
+                  <a href="#" 
+                  className="block px-4 py-2 text-gray-700 data:bg-gray-100">Settings
+                  </a>
+                </MenuItem>
+                <MenuItem>
+                  <a href="#" 
+                  className="block px-4 py-2 text-gray-700 data:bg-gray-100">Sign out
+                  </a>
+                </MenuItem>
+              </MenuItems>
+            </Menu>
           </div>
         </div>
       </div>
+
+      <DisclosurePanel className="sm:hidden">
+        <div className="space-y-1 px-2 pb-3 pt-2">
+          {navigation.map((item) => (
+            <DisclosureButton
+              key={item.name}
+              as="a"
+              href={item.href}
+              aria-current={item.current ? 'page' : undefined}
+              className={classNames(
+                item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                'block rounded-md px-3 py-2 text-base font-medium',
+            )}
+            >
+
+            </DisclosureButton>
+          ))}
+        </div>
+      </DisclosurePanel>
+
     </Disclosure>
   );
 }
